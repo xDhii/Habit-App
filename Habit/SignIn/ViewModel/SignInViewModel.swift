c25b352661452efa22c6275fae3a13979013c5ad
Adriano Valumin <adriano.valumin@outlook.com>
2023-04-15 21:46:41 -0300

    created first SignUpView, applied code formatting

--- Habit/SignIn/ViewModel/SignInViewModel.swift
+++ Habit/SignIn/ViewModel/SignInViewModel.swift
@@ -8,22 +8,23 @@
 import SwiftUI
 
 class SignInViewModel: ObservableObject {
-    
     @Published var uiState: SignInUIState = .none
-    
+
     func login(email: String, password: String) {
-        self.uiState = .loading
-        
+        uiState = .loading
+
         DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
             self.uiState = .goToHomeScreen
         }
     }
-    
-    
 }
 
 extension SignInViewModel {
     func homeView() -> some View {
         return SignInViewRouter.makeHomeView()
+    }
+
+    func signUpView() -> some View {
+        return SignInViewRouter.makeSignUpView()
     }
 }
