06ba4d0b9b4a1db1a279e6d7aea5c56407452091
Adriano Valumin <adriano.valumin@outlook.com>
2023-04-21 17:15:01 -0300

    changed some colors, created file for common components

---
+++ Habit/Common/View/EditTextView.swift
@@ -0,0 +1,8 @@
+//
+//  EditTextView.swift
+//  Habit
+//
+//  Created by Adriano Valumin on 20/04/23.
+//
+
+import Foundation
---
+++ Habit/Common/View/Style/CustomTextFieldStyle.swift
@@ -0,0 +1,8 @@
+//
+//  CustomTextFieldStyle.swift
+//  Habit
+//
+//  Created by Adriano Valumin on 20/04/23.
+//
+
+import Foundation
