ba71a9483c941266238a6acd604e6b437781ef23
Adriano Valumin <adriano.valumin@outlook.com>
2023-04-21 17:15:43 -0300

    updated email textfield to common component style

--- Habit/Common/View/EditTextView.swift
+++ Habit/Common/View/EditTextView.swift
@@ -5,4 +5,35 @@
 //  Created by Adriano Valumin on 20/04/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct EditTextView: View {
+    @Binding var text: String
+    var placeholder: String = ""
+    var keyboard: UIKeyboardType = .default
+    var error: String? = nil
+    var failure: Bool? = nil
+
+    var body: some View {
+        VStack {
+            TextField(placeholder, text: $text)
+                .keyboardType(keyboard)
+                .textFieldStyle(CustomTextFieldStyle())
+
+            if let error = error, failure == true, !text.isEmpty {
+                Text(error)
+                    .foregroundColor(.red)
+            }
+        }
+        .padding(.bottom, 10)
+    }
+}
+
+struct EditTextView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditTextView(text: .constant("Texto"),
+                     placeholder: "E-mail",
+                     error: "Campo inválido",
+                     failure: "a@a.com".count < 3)
+    }
+}
--- Habit/Common/View/Style/CustomTextFieldStyle.swift
+++ Habit/Common/View/Style/CustomTextFieldStyle.swift
@@ -5,4 +5,16 @@
 //  Created by Adriano Valumin on 20/04/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct CustomTextFieldStyle: TextFieldStyle {
+    public func _body(configuration: TextField<Self._Label>) -> some View {
+        configuration
+            .padding(.horizontal, 8)
+            .padding(.vertical, 16)
+            .overlay(
+                RoundedRectangle(cornerRadius: 8.0)
+                    .stroke(Color("borderColor"), lineWidth: 0.8)
+            )
+    }
+}
