e8648e1684296778b44f730ca13cb4dd31cf6d56
Adriano Valumin <adriano.valumin@outlook.com>
2023-04-22 20:47:41 -0300

    created colors for dynamic button :

---
+++ Habit/Common/View/LoadingButtonView.swift
@@ -0,0 +1,20 @@
+//
+//  LoadingButtonView.swift
+//  Habit
+//
+//  Created by Adriano Valumin on 21/04/23.
+//
+
+import SwiftUI
+
+struct LoadingButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoadingButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingButtonView()
+    }
+}
