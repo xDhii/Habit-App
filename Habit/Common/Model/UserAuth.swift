d38ac372e8da1cf7199867500f2adc122c21a579
Adriano Valumin <adriano.valumin@outlook.com>
2023-05-25 19:39:47 -0300

    get user token and save locally

---
+++ Habit/Common/Model/UserAuth.swift
@@ -0,0 +1,22 @@
+//
+//  UserAuth.swift
+//  Habit
+//
+//  Created by Adriano Valumin on 25/05/23.
+//
+
+import Foundation
+
+struct UserAuth: Codable {
+    var idToken: String
+    var refreshToken: String
+    var expires: Int = 0
+    var tokenType: String
+
+    enum CodingKeys: String, CodingKey {
+        case idToken = "access_token"
+        case refreshToken = "refresh_token"
+        case expires
+        case tokenType = "token_type"
+    }
+}
