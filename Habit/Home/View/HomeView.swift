ee1d32e8748ff7fafaec88de735ea46caeb7dc05
Adriano Valumin <adriano.valumin@outlook.com>
2023-06-02 22:45:15 -0300

    Created a divider between tabs

--- Habit/Home/View/HomeView.swift
+++ Habit/Home/View/HomeView.swift
@@ -11,21 +11,22 @@ struct HomeView: View {
     @ObservedObject var viewModel: HomeViewModel
 
     @State var selection = 0
-    
+
     var body: some View {
         TabView(selection: $selection) {
+//          Habits Tab
             viewModel.habitView()
                 .tabItem {
                     Image(systemName: "square.grid.2x2")
                     Text("Habits")
                 }.tag(0)
-            
+//          Charts Tab
             Text("Charts Content \(selection)")
                 .tabItem {
                     Image(systemName: "chart.bar")
                     Text("Charts")
                 }.tag(1)
-            
+//          Profile Tab
             Text("Profile Content \(selection)")
                 .tabItem {
                     Image(systemName: "person.crop.circle")
