622942b2bd8d5fda29978861f1d01172aa047fec
Adriano Valumin <adriano.valumin@outlook.com>
2023-06-04 20:57:57 -0300

    Created generic modifier for buttons

---
+++ Habit/Common/View/Modifier/ButtonStyle.swift
@@ -0,0 +1,22 @@
+//
+//  ButtonStyle.swift
+//  Habit
+//
+//  Created by Adriano Valumin on 04/06/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct ButtonStyle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .frame(maxWidth: .infinity)
+            .padding(.vertical, 14)
+            .padding(.horizontal, 16)
+            .font(Font.system(.title3).bold())
+            .foregroundColor(Color("primaryText"))
+            .background(Color("PrimaryButton"))
+            .cornerRadius(7.0)
+    }
+}
