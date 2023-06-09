523aeaad7f1393aac041561220300acb169baba9
Adriano Valumin <adriano.valumin@outlook.com>
2023-06-09 18:32:50 -0300

    Removed tests for background color

--- Habit/Home/View/HomeView.swift
+++ Habit/Home/View/HomeView.swift
@@ -13,35 +13,29 @@ struct HomeView: View {
     @State var selection = 0
 
     var body: some View {
-        ZStack {
-            ZStack(alignment: .top) {
-                Color("primaryBackgroundColor")
-                    .ignoresSafeArea() //TODO: Check why background color is not being applied
-                TabView(selection: $selection) {
-                    //          Habits Tab
-                    viewModel.habitView()
-                        .tabItem {
-                            Image(systemName: "square.grid.2x2")
-                            Text("Habits")
-                            
-                        }.tag(0)
-                    //          Charts Tab
-                    Text("Charts Content \(selection)")
-                        .tabItem {
-                            Image(systemName: "chart.bar")
-                            Text("Charts")
-                        }.tag(1)
-                    //          Profile Tab
-                    Text("Profile Content \(selection)")
-                        .tabItem {
-                            Image(systemName: "person.crop.circle")
-                            Text("Profile")
-                        }.tag(2)
-                }
-                .background(Color("secondaryBackgroundColor"))
-                .accentColor(Color("tabViewIcon"))
-            }
+        TabView(selection: $selection) {
+            //          Habits Tab
+            viewModel.habitView()
+                .tabItem {
+                    Image(systemName: "square.grid.2x2")
+                    Text("Habits")
+
+                }.tag(0)
+            //          Charts Tab
+            Text("Charts Content \(selection)")
+                .tabItem {
+                    Image(systemName: "chart.bar")
+                    Text("Charts")
+                }.tag(1)
+            //          Profile Tab
+            Text("Profile Content \(selection)")
+                .tabItem {
+                    Image(systemName: "person.crop.circle")
+                    Text("Profile")
+                }.tag(2)
         }
+        .background(Color("secondaryBackgroundColor"))
+        .accentColor(Color("tabViewIcon"))
     }
 }
 
