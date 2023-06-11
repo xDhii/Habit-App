b63cd929ddfb89c09fcf5457739d51a1ae608606
Adriano Valumin <adriano.valumin@outlook.com>
2023-06-10 21:19:07 -0300

    Created habit details

---
+++ Habit/HabitDetail/Model/HabitDetailUIState.swift
@@ -0,0 +1,15 @@
+//
+//  HabitDetailUIState.swift
+//  Habit
+//
+//  Created by Adriano Valumin on 10/06/23.
+//
+
+import Foundation
+
+enum HabitDetailUIState: Equatable {
+    case none
+    case loading
+    case success
+    case error(String)
+}
