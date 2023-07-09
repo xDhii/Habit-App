72ce27d51dd2a4f119948400eedc3ce06d663b40
Adriano Valumin <adriano.valumin@outlook.com>
2023-07-09 19:41:56 -0300

    Fixed wrong validation

--- Habit/Profile/ViewModel/ProfileViewModel.swift
+++ Habit/Profile/ViewModel/ProfileViewModel.swift
@@ -130,7 +130,7 @@ class PhoneValidation: ObservableObject {
 
   var value: String = "11999999999" {
     didSet {
-      failure = value.count < 14 || value.count >= 15
+      failure = value.count < 14 || value.count > 15
     }
   }
 }
